f12672cbd12a6a32bea3792ad96faa225a525c83
Joris Gillis <joris.gillis42@gmail.com>
2013-03-01 14:54:15 +0100

    improving installer again

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
+this=`pwd`
+echo "export PATH=\$PATH:$this" > bashrc
+echo "export MANPATH=\$MANPATH:$this/man" >> bashrc
 export PATH="$PATH:."
 registerbashrc bashrc
 sudo aptitude -y install txt2man
